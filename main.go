804078ab61f1cd7d7ac53827f277c35ec3aa390b
James Raden <james.raden@gmail.com>
2021-02-06 22:14:16 -0500

    Add initDatabase()

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/sidecut/go-fiber-app/book"
+	"github.com/sidecut/go-fiber-app/database"
 )
 
 func helloWorld(c *fiber.Ctx) error {
@@ -18,9 +23,21 @@ func setupRoutes(app *fiber.App) {
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+}
+
 func main() {
 	app := fiber.New()
+	initDatabase()
 
 	setupRoutes(app)
 	app.Listen(":3000")
+
+	defer database.DBConn.Close()
 }
